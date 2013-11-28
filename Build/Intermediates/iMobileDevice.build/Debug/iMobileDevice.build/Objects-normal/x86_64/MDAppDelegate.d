f896d9bd307634c1b5c11f8917516a91d255b92a
Ha Duyen Hoa <tyt_g207@yahoo.com>
2013-11-28 17:32:31 +0100

    remove some necessaries files

---
+++ Build/Intermediates/iMobileDevice.build/Debug/iMobileDevice.build/Objects-normal/x86_64/MDAppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/MY_DATA/PERSONNEL/MY_RESEARCHS/iOS7/iDeviceManager/iMobileDevice/MDAppDelegate.m \
+  /Volumes/MY_DATA/PERSONNEL/MY_RESEARCHS/iOS7/iDeviceManager/iMobileDevice/MDAppDelegate.h
---
+++ Build/Intermediates/iMobileDevice.build/Debug/iMobileDevice.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Volumes/MY_DATA/PERSONNEL/MY_RESEARCHS/iOS7/iDeviceManager/iMobileDevice/main.m
