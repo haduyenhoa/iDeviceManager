1a58c5f4b7c48839f68a6ef28f07b5b06cf6b348
Ha Duyen Hoa <tyt_g207@yahoo.com>
2013-12-05 11:53:36 +0100

    remove unnecessaries files

--- DerivedData/iMobileDevice/Build/Intermediates/iMobileDevice.build/Debug/iMobileDevice.build/Objects-normal/x86_64/MDAppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/DATA/MY_WORKS/iOS7/iDeviceManager/iMobileDevice/MDAppDelegate.m \
-  /Volumes/DATA/MY_WORKS/iOS7/iDeviceManager/iMobileDevice/MDAppDelegate.h
--- DerivedData/iMobileDevice/Build/Intermediates/iMobileDevice.build/Debug/iMobileDevice.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Volumes/DATA/MY_WORKS/iOS7/iDeviceManager/iMobileDevice/main.m
